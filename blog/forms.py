2a2dcf48c41af3f5cd6bae1a30fc9fe5c9588760
wwq0327 <wwq0327@gmail.com>
2011-12-11 13:02:22 +0800

    css

--- blog/forms.py
+++ blog/forms.py
@@ -5,11 +5,11 @@ from django import forms
 #from blog.models import Entry
 
 class EntryForm(forms.Form):
-    title = forms.CharField(label="标题", widget=forms.TextInput(attrs={'size': 100}))
+    title = forms.CharField(label="标题", widget=forms.TextInput(attrs={'size': 68}))
 
     content = forms.CharField(label="内容", widget=forms.Textarea(attrs={'cols':80, 'rows':12}))
 
-    tags = forms.CharField(label="标签", required=False, widget=forms.TextInput(attrs={'size': 100}))
+    tags = forms.CharField(label="标签", required=False, widget=forms.TextInput(attrs={'size': 68}))
 
     ## class Meta:
     ##     model = Entry
