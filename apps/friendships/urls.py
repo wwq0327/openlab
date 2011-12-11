fd6d52b304e078652edbca269926e1636f9209ef
wwq0327 <wwq0327@gmail.com>
2011-12-11 15:45:18 +0800

    friends

--- apps/friendships/urls.py
+++ apps/friendships/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('openlab.apps.friendships.views',
                        url(r'^$', 'friends_page'),
                        url(r'^add/$', 'friend_add'),
+                       url(r'^unfollow/$', 'unfollow'),
                        )
--- apps/friendships/views.py
+++ apps/friendships/views.py
@@ -9,6 +9,7 @@ from django.core.urlresolvers import reverse
 from django.core.paginator import Paginator
 
 from django.contrib.auth.models import User
+from openlab.apps.friendships.models import Friendship
 
 @login_required
 def friends_page(request, username):
@@ -23,13 +24,30 @@ def friends_page(request, username):
     return render_to_response('friendships/friends_page.html', var)
 
 @login_required
-def friend_add(request):
-    if request.GET.has_key('username'):
-        friend = get_object_or_404(User, username=request.GET['username'])
-        friendship = Friendship(from_friend=request.user,
-                                to_friend=friend)
-        friendship.save()
-        return HttpResponseRedirect('/%s/friends/' & request.user.username)
-    else:
-        raise Http404
+def friend_add(request, username):
+    friend = get_object_or_404(User, username=username)
+    friendship = Friendship(from_friend=request.user,
+                            to_friend=friend)
+    friendship.save()
+
+    return HttpResponseRedirect('/%s/home/' % username)
+
+
+    ## if request.GET.has_key('username'):
+    ##     friend = get_object_or_404(User, username=request.GET['username'])
+    ##     friendship = Friendship(from_friend=request.user,
+    ##                             to_friend=friend)
+    ##     friendship.save()
+    ##     return HttpResponseRedirect('/%s/friends/' & request.GET['username'])
+    ## else:
+    ##     raise Http404
+
+@login_required
+def unfollow(request, username):
+    friend = get_object_or_404(User, username=username)
+    q_f = get_object_or_404(Friendship, to_friend=friend)
+
+    q_f.delete()
+
+    return  HttpResponseRedirect('/%s/home/' % username)
 
